264ea81e76d206b4cb5867358a3a392436acb130
kiranravindrant <kiranravindrant@gmail.com>
2021-08-01 16:26:49 +0530

    [kiran]Program file Refactored with annotations

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration_With_Annotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            ValidateUser.Display();
+        }
+    }
+}
